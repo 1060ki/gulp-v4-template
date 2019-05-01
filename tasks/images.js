503d41155056de9eabea745cd5c5273e21cf7768
Tomoki Ichikawa <ichikawa@1060ki.com>
2019-05-01 18:57:33 +0900

    fix images task

--- tasks/images.js
+++ tasks/images.js
@@ -10,7 +10,7 @@ export const images = () => {
     .src(config.images.src, { since: gulp.lastRun(images) })
     .pipe(imagemin(
       [
-        pngquant({ quality: '65-80', speed: 1 }),
+        pngquant({ quality: [.65, .85], speed: 1 }),
         mozjpeg({ quality: 80 }),
         imagemin.svgo(),
         imagemin.gifsicle()
